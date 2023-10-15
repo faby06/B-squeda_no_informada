d7ed3af72210a94647d46dd45d400290d5667ee3
faby06 <143236825+faby06@users.noreply.github.com>
2023-10-14 21:12:47 -0600

    Update _06_Búsqueda_Bidireccional.py

--- _06_Búsqueda_Bidireccional.py
+++ _06_Búsqueda_Bidireccional.py
@@ -1,1 +1,33 @@
+#Busqueda bidireccional 
+def busqueda_bidireccional(grafo, nodo_inicial, nodo_objetivo):
+    conjunto_inicio = set([nodo_inicial])
+    conjunto_objetivo = set([nodo_objetivo])
 
+    while conjunto_inicio and conjunto_objetivo:
+        interseccion = conjunto_inicio & conjunto_objetivo
+        if interseccion:
+            return list(interseccion)
+
+        conjunto_inicio = {vecino for nodo in conjunto_inicio for vecino in grafo.get(nodo, [])}
+        conjunto_objetivo = {vecino for nodo in conjunto_objetivo for vecino in grafo.get(nodo, [])}
+
+    return None
+
+# Ejemplo de uso:
+grafo = {
+    'A': ['B', 'C'],
+    'B': ['A', 'D'],
+    'C': ['A', 'E'],
+    'D': ['B', 'E'],
+    'E': ['C', 'D', 'F'],
+    'F': ['E']
+}
+
+nodo_inicial = 'A'
+nodo_objetivo = 'E'
+
+camino = busqueda_bidireccional(grafo, nodo_inicial, nodo_objetivo)
+if camino:
+    print("Camino encontrado:", " -> ".join(camino))
+else:
+    print("No se encontro un camino.")
